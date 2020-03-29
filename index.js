0f4a5f6c10e959b4001430a7da69032941b38e2f
Michael Peterson <michaeljoepeterson@gmail.com>
2020-03-29 16:31:33 -0600

    built initial instagram data converter

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
+function instagramDataConvert(sourceData,feedLength){
+    feedLength = feedLength ? feedLength : 10;
+    let data = [];
+    let dataStructure = {
+        link:'',
+        images:{
+            standard_resolution:{
+                url:''
+            }
+        }
+    };
+
+    for(let i = 0;i < feedLength;i++){
+        let singleSrc = sourceData[i];
+        let singleData = {
+            link:singleSrc.permalink,
+            images:{
+                standard_resolution:{
+                    url:singleSrc.media_url
+                }
+            }
+        };
+        data.push(singleData);
+    }
+
+    return data;
+}
+
 function getInsta(){
+    let feedLength = 11;
+
     let options = {
         method:'GET',
-        url:'https://graph.instagram.com/17841401954357107?fields=id,username&access_token=IGQVJYVDE4UjQ5clFENWR0U29ZATkpta3piOG1mUm9aY0RLZAU5XM2ZAMZAEdiSWUtbzVxaGV0dkJTVU90bVBjaWx1VGFQc1NBcFVQSmtQOFZAGc0RZAdHBRUFVEQVNnMUtqWnVFcjdsb1BVRVFsRlU1MHJDT3ZAMZAXFJRS12ZAUZA3'
+        url:'https://graph.instagram.com/17841401297140449/media?fields=id,caption,media_type,media_url,permalink&access_token=IGQVJWR3JOdV9wLXZAXdEFjd2QzaDdSZAGctRThCREpWTHNPcWc2ZAmZAyYktDeE5GTGFRSWgyQUtLWXJId0VNOHJsWm5ZAaGtqWWZAXOFZAjZAE1yX0N6VXVKZA3dPQkdJTVpnYV9CTGRFeFZApNnlIQ3l5MlJjLXNkUnY5eElfaFdv'
     };
     $.ajax(options)
 
     .then(response => {
+        let data = instagramDataConvert(response.data,feedLength);
         console.log('insta resp: ', response);
+        console.log('insta converted: ', data);
     })
 
     .catch(err => {
         console.log('error getting: ',err);
     });
 }
+
+//likely have to change urls in current site
+//need to test to see what data comes back
+//need to test req with old args
+//figure out how long key lasts
 
 getInsta();
